a0af34ae0782c3197a04576b8135da41cddda42b
James Klein <kleinjm007@gmail.com>
2025-10-01 16:05:34 +0000

    Fix gprune function to properly delete stale branches
    The previous implementation had issues with grep filtering logic that caused it to fail with "fatal: branch name required". Simplified to use git's built-in tracking status to identify and delete local branches whose remote tracking branches have been removed.

--- zsh/git_functions.zsh
+++ zsh/git_functions.zsh
@@ -1,11 +1,6 @@
 # delete local branches that do not exist on origin
 gprune() {
-  git prune &&
-    git fetch --prune &&
-    git branch -r |
-    awk '{print $1}' |
-    egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) |
-    awk '{print $1}' | xargs git branch -D
+  git fetch --prune && git branch -vv | grep ': gone]' | awk '{print $1}' | xargs -r git branch -D
 }
 
 # pull the latest remote changes for this branch and merge latest remote master
